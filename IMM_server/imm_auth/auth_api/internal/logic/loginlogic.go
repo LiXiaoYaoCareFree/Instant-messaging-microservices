0e470b992fc96b6562f94ff0918300556c5f07e4
LiXiaoYaoCareFree <2210127151@qq.com>
2025-04-04 23:55:34 +0800

    go-zero log configuration + Goland password handling

--- IMM_server/imm_auth/auth_api/internal/logic/loginlogic.go
+++ IMM_server/imm_auth/auth_api/internal/logic/loginlogic.go
@@ -26,5 +26,5 @@ func NewLoginLogic(ctx context.Context, svcCtx *svc.ServiceContext) *LoginLogic 
 func (l *LoginLogic) Login(req *types.LoginRequest) (resp *types.LoginResponse, err error) {
 	// todo: add your logic here and delete this line
 
-	return
+	return &types.LoginResponse{Token: "xxx"}, nil
 }
