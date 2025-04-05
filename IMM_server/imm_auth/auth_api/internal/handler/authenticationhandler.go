424c1a017ad0383d0fc83bd1116cdc88aade66c3
LiXiaoYaoCareFree <2210127151@qq.com>
2025-04-05 16:22:39 +0800

    Authentication Service - Authentication Interface API

--- IMM_server/imm_auth/auth_api/internal/handler/authenticationhandler.go
+++ IMM_server/imm_auth/auth_api/internal/handler/authenticationhandler.go
@@ -9,10 +9,9 @@ import (
 
 func authenticationHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-
-		l := logic.NewAuthenticationLogic(r.Context(), svcCtx)
-		resp, err := l.Authentication()
-		response.Response(r, w, resp, err)
-
+		l := logic.NewAuthenticationLogic(r.Context(), svcCtx) // 接收客户端请求
+		token := r.Header.Get("token")                         // 从请求头中获取 token
+		resp, err := l.Authentication(token)                   // 调用逻辑层的 Authentication() 方法处理认证
+		response.Response(r, w, resp, err)                     // 返回结果给客户端
 	}
 }
--- IMM_server/imm_auth/auth_api/internal/logic/authenticationlogic.go
+++ IMM_server/imm_auth/auth_api/internal/logic/authenticationlogic.go
@@ -1,7 +1,10 @@
 package logic
 
 import (
+	"IMM_server/utils/jwts"
 	"context"
+	"errors"
+	"fmt"
 
 	"IMM_server/imm_auth/auth_api/internal/svc"
 	"github.com/zeromicro/go-zero/core/logx"
@@ -21,8 +24,25 @@ func NewAuthenticationLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Au
 	}
 }
 
-func (l *AuthenticationLogic) Authentication() (resp string, err error) {
-	// todo: add your logic here and delete this line
+func (l *AuthenticationLogic) Authentication(token string) (resp string, err error) {
+	if token == "" { // 空 Token 校验
+		err = errors.New("认证失败")
+		return
+	}
 
+	payload, err := jwts.ParseToken(token, l.svcCtx.Config.Auth.AccessSecret) // 使用 jwts.ParseToken 解密 Token
+	if err != nil {
+		err = errors.New("认证失败")
+		return
+	}
+
+	_, err = l.svcCtx.Redis.Get(fmt.Sprintf("logout_%d", payload.UserID)).Result() // 如果用户已登出，则 Redis 会保存 logout_用户ID 的 key
+	if err == nil {
+		err = errors.New("认证失败")
+		return
+	}
+
+	resp = "ok"
+	err = nil
 	return
 }
