515c9ae413fb6a158df011e5d2dcd5a6786457f0
LiXiaoYaoCareFree <2210127151@qq.com>
2025-04-05 15:08:03 +0800

    Authentication Service-Logout API

--- IMM_server/imm_auth/auth_api/internal/logic/logoutlogic.go
+++ IMM_server/imm_auth/auth_api/internal/logic/logoutlogic.go
@@ -1,7 +1,11 @@
 package logic
 
 import (
+	"IMM_server/utils/jwts"
 	"context"
+	"errors"
+	"fmt"
+	"time"
 
 	"IMM_server/imm_auth/auth_api/internal/svc"
 	"github.com/zeromicro/go-zero/core/logx"
@@ -21,8 +25,23 @@ func NewLogoutLogic(ctx context.Context, svcCtx *svc.ServiceContext) *LogoutLogi
 	}
 }
 
-func (l *LogoutLogic) Logout() (resp string, err error) {
-	// todo: add your logic here and delete this line
+func (l *LogoutLogic) Logout(token string) (resp string, err error) {
+	if token == "" {
+		err = errors.New("请传入token")
+		return
+	}
 
+	payload, err := jwts.ParseToken(token, l.svcCtx.Config.Auth.AccessSecret)
+	if err != nil {
+		err = errors.New("token错误")
+		return
+	}
+	now := time.Now()
+	// 过期时间就是这个jwt的失效时间
+	expiration := payload.ExpiresAt.Time.Sub(now)
+
+	key := fmt.Sprintf("logout_%d", payload.UserID)
+	l.svcCtx.Redis.Set(key, token, expiration)
+	resp = "注销成功"
 	return
 }
--- IMM_server/imm_auth/auth_api/internal/svc/servicecontext.go
+++ IMM_server/imm_auth/auth_api/internal/svc/servicecontext.go
@@ -3,19 +3,22 @@ package svc
 import (
 	"IMM_server/core"
 	"IMM_server/imm_auth/auth_api/internal/config"
+	"github.com/go-redis/redis"
 	"gorm.io/gorm"
 )
 
 type ServiceContext struct {
 	Config config.Config
 	DB     *gorm.DB
+	Redis  *redis.Client
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	mysqlDb := core.InitGorm(c.Mysql.DataSource)
-	//mysqlDb.AutoMigrate(&auth_models.UserModel{})
+	client := core.InitRedis(c.Redis.Addr, c.Redis.Pwd, c.Redis.DB)
 	return &ServiceContext{
 		Config: c,
 		DB:     mysqlDb,
+		Redis:  client,
 	}
 }
