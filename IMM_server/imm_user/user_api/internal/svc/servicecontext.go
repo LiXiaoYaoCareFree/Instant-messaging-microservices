3fa5ffdaf5006c3bfce7692e4bfcf221cbec02fb
LiXiaoYaoCareFree <2210127151@qq.com>
2025-04-07 23:15:23 +0800

    User Service - Modification of user information

--- IMM_server/imm_user/user_api/internal/svc/servicecontext.go
+++ IMM_server/imm_user/user_api/internal/svc/servicecontext.go
@@ -1,20 +1,25 @@
 package svc
 
 import (
+	"IMM_server/core"
 	"IMM_server/imm_user/user_api/internal/config"
 	"IMM_server/imm_user/user_rpc/types/user_rpc"
 	"IMM_server/imm_user/user_rpc/users"
 	"github.com/zeromicro/go-zero/zrpc"
+	"gorm.io/gorm"
 )
 
 type ServiceContext struct {
 	Config  config.Config
+	DB      *gorm.DB
 	UserRpc user_rpc.UsersClient
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
+	mysqlDb := core.InitGorm(c.Mysql.DataSource)
 	return &ServiceContext{
 		Config:  c,
+		DB:      mysqlDb,
 		UserRpc: users.NewUsers(zrpc.MustNewClient(c.UserRpc)),
 	}
 }
